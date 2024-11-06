00daa0585931e53ab27d7d69838c0e99e4963bbc
Bo Fethe <bofethe@gmail.com>
2024-11-05 22:20:31 -0500

    added vote delta

--- election.py
+++ election.py
@@ -16,8 +16,10 @@ df_electoral = pd.read_csv('data/statevotes.csv')
 # Merge the 2 dfs
 df = df_pop.merge(df_electoral, on=['NAME'])
 
-# calcualte individual vote weight
+# calcualte individual vote weight and fair number of votes based on population
 df['VOTE_WEIGHT'] = df['NUM_VOTES'] / df['POP']
+df['FAIR_NUM_VOTES'] = df['NUM_VOTES'].sum() / df['POP'].sum() * df['POP']
+df['VOTE_DELTA'] = df['NUM_VOTES'] - df['FAIR_NUM_VOTES']
 
 # Sort the df by weight
 df = df.sort_values(by=['VOTE_WEIGHT'], ascending=False)
@@ -30,7 +32,7 @@ gdf = gpd.read_file('data/US_State_Boundaries/US_State_Boundaries.shp')
 gdf_merge = gdf.merge(df)
 
 # Create a figure with two subplots
-fig, axes = plt.subplots(2, 1, figsize=(4, 4))
+fig, axes = plt.subplots(3, 1, figsize=(10, 10))
 fig.patch.set_facecolor('lightgray')
 
 # Plot the first map (using default classification)
@@ -44,6 +46,13 @@ gdf_merge.plot(column='VOTE_WEIGHT', ax=axes[1], scheme='quantiles',
                cmap='YlOrRd')
 axes[1].set_title('Voter Weight (Quantile)', loc='center')
 axes[1].set_axis_off()
+
+# Plot the second map (using quantiles classification)
+gdf_merge.plot(column='VOTE_DELTA', ax=axes[2], legend=True,
+               legend_kwds={"orientation": "horizontal", "pad": 0.01},
+               cmap='coolwarm')
+axes[2].set_title('Under/Over Electoral Representation', loc='center')
+axes[2].set_axis_off()
 
 # Adjust layout to ensure maps don't overlap
 plt.tight_layout()
